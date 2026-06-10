c4467bb27e80df2d50b46073875d0f8e8a031733
mariavitoriamartins11 <maria.vitoria.martins11@escola.pr.gov.br>
2026-06-10 13:34:24 -0300

    Update script.js

--- img/script.js
+++ img/script.js
@@ -1,1 +1,39 @@
+function calcularConsumo(){
 
+    const pessoas = Number(document.getElementById("pessoas").value);
+    const banho = Number(document.getElementById("banho").value);
+    const descarga = Number(document.getElementById("descarga").value);
+
+    const resultado = document.getElementById("resultado");
+
+    if(!pessoas || !banho || !descarga){
+        resultado.innerHTML = "⚠️ Preencha todos os campos.";
+        return;
+    }
+
+    const litrosBanho = banho * 12;
+    const litrosDescarga = descarga * 6;
+
+    const consumoPessoa = litrosBanho + litrosDescarga;
+
+    const consumoDiario = consumoPessoa * pessoas;
+    const consumoMensal = consumoDiario * 30;
+
+    let classificacao;
+
+    if(consumoDiario < 300){
+        classificacao = "🌱 Excelente consumo!";
+    }else if(consumoDiario < 600){
+        classificacao = "💧 Consumo moderado.";
+    }else{
+        classificacao = "⚠️ Consumo alto. Procure economizar água.";
+    }
+
+    resultado.innerHTML = `
+        <h3>Resultado</h3>
+        <p><strong>Consumo diário:</strong> ${consumoDiario.toFixed(0)} litros</p>
+        <p><strong>Consumo mensal:</strong> ${consumoMensal.toFixed(0)} litros</p>
+        <br>
+        <p>${classificacao}</p>
+    `;
+}
